f228485e66198f02723dc3e251aa138d605b98df
JF MIYOX <169084103+jfmiyox@users.noreply.github.com>
2026-05-24 22:55:10 -0500

    Nueva

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://eventorproducciones-lht5pcic7-sandredev-projects.vercel.app", 
+    "https://eventorproducciones.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
